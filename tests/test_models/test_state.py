2f5f7dd8047e5d4a0163b9a9583404eb019e0f50
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-08-23 01:24:05 +0300

    added testcases for city, place, review, state, and user

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,6 +2,7 @@
 """ Module for testing state """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+import unittest
 
 
 class test_state(test_basemodel):
@@ -17,3 +18,50 @@ class test_state(test_basemodel):
         """ Test State Name """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+
+class TestState2(unittest.TestCase):
+    """TestState Class"""
+    def setUp(self):
+        """Sets up State for testing"""
+        self.state = State()
+
+    def test_state_type(self):
+        """Tests state type"""
+        self.assertEqual(type(self.state.name), str)
+
+    def test_state_name(self):
+        """Tests state name"""
+        self.assertEqual(self.state.name, "")
+
+    def test_state_id(self):
+        """Tests state id"""
+        self.assertEqual(type(self.state.id), str)
+
+    def test_state_created_at(self):
+        """Tests state created_at"""
+        self.assertEqual(type(self.state.created_at).__name__, "datetime")
+
+    def test_state_updated_at(self):
+        """Tests state updated_at"""
+        self.assertEqual(type(self.state.updated_at).__name__, "datetime")
+
+    def test_state_str(self):
+        """Tests state __str__"""
+        self.assertEqual(type(self.state.__str__()), str)
+
+    def test_state_save(self):
+        """Tests state save"""
+        self.state.save()
+        self.assertEqual(type(self.state.updated_at).__name__, "datetime")
+
+    def test_state_to_dict(self):
+        """Tests state to_dict"""
+        self.assertEqual(type(self.state.to_dict()), dict)
+
+    # def test_state_kwargs(self):
+    #     """Tests state kwargs"""
+    #     self.new_state = State(name="California")
+    #     self.assertEqual(type(self.new_state).__name__, "State")
+    #     self.assertTrue(hasattr(self.new_state, "name"))
+    #     self.assertEqual(self.new_state.name, "California")
