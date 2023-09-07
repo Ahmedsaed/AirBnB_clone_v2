3ee3ff98eb28da99608d3352ef4c978e8e23686e
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-07 17:28:47 +0300

    deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,23 +13,23 @@ def do_depoly(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, f"/tmp/{file}").failed is True:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run(f"rm -rf /data/web_static/releases/{name}/").failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
         return False
-    if run(f"mkdir -p /data/web_static/releases/{name}/").failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
         return False
-    if run(f"tar -xzf /tmp/{file} -c /data/web_static/releases/{name}/").failed is True:
+    if run("tar -xzf /tmp/{} -c /data/web_static/releases/{}/".format(file,name)).failed is True:
         return False
-    if run(f"rm /tmp/{file}").failed is True:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    if run(f"mv /data/web_static/releases/{name}/web_static/* "
-           f"/data/web_static/release/{name}/").failed is True:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/release/{}/".format(name,name)).failed is True:
         return False
-    if run(f"rm -rf /data/web_static/releases/{name}/web_static").failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
         return False
-    if run(f"rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run(f"ln -s /data/web_static/releases/{name}/ /data/web_static/current").failed is True:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
         return False
     return True
