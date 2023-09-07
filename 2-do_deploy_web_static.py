108430dc8e53bbad790d0805075cceef42a6d695
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-07 17:07:36 +0300

    deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,33 +3,33 @@
 import os.path
 from fabric.api import env,put,run
 
-env_hosts = ["100.25.14.86","54.236.115.46"]
-
+env_hosts = ["34.202.157.122","100.27.2.78"]
 
 def do_depoly(archive_path):
     """Distributes archive to web server"""
     if os.path.isfile(archive_path) is False:
         return False
+
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if put(archive_path, f"/tmp/{file}").failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
+    if run(f"rm -rf /data/web_static/releases/{name}/").failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
+    if run(f"mkdir -p /data/web_static/releases/{name}/").failed is True:
         return False
-    if run("tar -xzf /tmp/{} -c /data/web_static/releases/{}/".format(file,name)).failed is True:
+    if run(f"tar -xzf /tmp/{file} -c /data/web_static/releases/{name}/").failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run(f"rm /tmp/{file}").failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web/web_static/release/{}/".format(name,name)).failed is True:
+    if run(f"mv /data/web_static/releases/{name}/web_static/* "
+           f"/data/web_static/release/{name}/").failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
+    if run(f"rm -rf /data/web_static/releases/{name}/web_static").failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run(f"rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
+    if run(f"ln -s /data/web_static/releases/{name}/ /data/web_static/current").failed is True:
         return False
     return True
