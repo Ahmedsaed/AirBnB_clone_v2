0533888482fd7bdec6d48515eff221f7cf9b565e
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-07 16:32:43 +0300

    handle failed commands in pack_web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,12 +11,11 @@ from os.path import isdir
 
 def do_pack():
     """generates a TGZ archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("version") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local(f"tar -cvzf {file_name} web_static")
-        return file_name
-    except:
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    if isdir("version") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    file_name = "versions/web_static_{}.tgz".format(date)
+    if local(f"tar -cvzf {file_name} web_static").failed is True:
         return None
+    return file_name
