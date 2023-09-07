004f3318551868ff9b4a8383eee3e79ba4f9e4b5
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-09-07 12:09:06 +0300

    changed html content & set-up servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 # Sets up the web servers for the deployement of web_static
 
-sudo apt-get -y update
+sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo echo '<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html' > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbtn_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
