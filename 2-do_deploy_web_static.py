6d80b95a0cba0697e9e406d130e1e54b1697b800
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-10 16:14:35 +0300

    run the script locally too

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,11 +4,13 @@ import os.path
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
+from fabric.api import local
 
 env.hosts = ["34.202.157.122", "100.27.2.78"]
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/id_rsa"
 
+run_locally = True
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
@@ -24,30 +26,42 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
+    if run_locally:
+        local("cp {} /tmp/{}".format(archive_path, file))
+        local("rm -rf /data/web_static/releases/{}/".format(name))
+        local("mkdir -p /data/web_static/releases/{}/".format(name))
+        local("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+        local("rm /tmp/{}".format(file))
+        local("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+        local("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        local("rm -rf /data/web_static/current")
+        local("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+        run_locally = False
+    else:
+        if put(archive_path, "/tmp/{}".format(file)).failed is True:
+            return False
+        if run("rm -rf /data/web_static/releases/{}/".
+            format(name)).failed is True:
+            return False
+        if run("mkdir -p /data/web_static/releases/{}/".
+            format(name)).failed is True:
+            return False
+        if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(file, name)).failed is True:
+            return False
+        if run("rm /tmp/{}".format(file)).failed is True:
+            return False
+        if run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name)).failed is True:
+            return False
+        if run("rm -rf /data/web_static/releases/{}/web_static".
+            format(name)).failed is True:
+            return False
+        if run("rm -rf /data/web_static/current").failed is True:
+            return False
+        if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+            format(name)).failed is True:
+            return False
 
     print("New version deployed!")
     return True
