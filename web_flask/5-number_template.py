2cd3005657579825537ae1f5eaab15779f8a7e3e
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-23 21:28:01 +0300

    number template api

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# simple flask app
+from flask import Flask, request
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return f'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return f'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text=None):
+    return f'C {text.replace("_", " ")}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    return f'Python {text.replace("_", " ")}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+
+app.run(debug=True)
