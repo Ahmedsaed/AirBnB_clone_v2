a276e04e6b4712ef1fdd2dcacfc95849f6db3376
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-23 19:16:36 +0300

    simple flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# simple flask app
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return f'Hello HBNB!'
+
+app.run(debug=True)
