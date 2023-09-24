a82ca825442dc2f70c5a650202169c0b5871d8ed
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-24 21:15:02 +0300

    restore orignal code

--- models/place.py
+++ models/place.py
@@ -18,7 +18,7 @@ if getenv("HBNB_TYPE_STORAGE", None) == 'db':
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    # city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
